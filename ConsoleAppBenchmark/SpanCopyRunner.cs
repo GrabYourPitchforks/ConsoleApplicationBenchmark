9fc7c2a40b1df5a5c82b7ce5b3ec7f7544a9d45c
Levi Broderick <levib@microsoft.com>
2019-05-01 13:16:28 -0700

    Add SpanCopy and Vector runners

---
+++ ConsoleAppBenchmark/SpanCopyRunner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace ConsoleAppBenchmark
+{
+    public class SpanCopyRunner
+    {
+        private byte[] _from4 = Enumerable.Range(0, 4).Select(i => (byte)i).ToArray();
+        private byte[] _from1024 = Enumerable.Range(0, 1024).Select(i => (byte)i).ToArray();
+        private byte[] _from1M = Enumerable.Range(0, 1000000).Select(i => (byte)i).ToArray();
+        private byte[] _to = new byte[1000000];
+
+        [Benchmark] public void CopySpan4() => _from4.AsSpan().CopyTo(_to);
+        [Benchmark] public void CopySpan1024() => _from1024.AsSpan().CopyTo(_to);
+        [Benchmark] public void CopySpan1M() => _from1M.AsSpan().CopyTo(_to);
+
+    }
+}
---
+++ ConsoleAppBenchmark/VectorRunner.cs
@@ -0,0 +1,70 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ConsoleAppBenchmark
+{
+    public unsafe class VectorRunner
+    {
+        private byte[] _bytes = new byte[Vector<byte>.Count];
+        private uint[] _uints = new uint[Vector<uint>.Count];
+
+        [Benchmark]
+        public uint DoIt()
+        {
+            return SumAndGetFirstElement(_bytes, _uints);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static uint SumAndGetFirstElement(ReadOnlySpan<byte> bytes, ReadOnlySpan<uint> uints)
+        {
+            return (new Vector<uint>(bytes) + new Vector<uint>(uints))[0];
+            // return (CreateVector<uint>(bytes) + CreateVector<uint>(uints))[0];
+        }
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //private static Vector<T> CreateVector<T>(ReadOnlySpan<byte> bytes) where T : struct
+        //{
+        //    Vector<T> retVal;
+
+        //    ThrowForUnsupportedVectorBaseType<T>();
+        //    if (!MemoryMarshal.TryRead(bytes, out retVal))
+        //    {
+        //        ThrowSomething(Vector<byte>.Count);
+        //    }
+
+        //    return retVal;
+        //}
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //private static Vector<T> CreateVector<T>(ReadOnlySpan<T> ts) where T : struct
+        //{
+        //    ThrowForUnsupportedVectorBaseType<T>();
+        //    if (ts.Length < Vector<T>.Count)
+        //    {
+        //        ThrowSomething(Vector<T>.Count);
+        //    }
+        //    return Unsafe.ReadUnaligned<Vector<T>>(ref Unsafe.As<T, byte>(ref MemoryMarshal.GetReference(ts)));
+        //}
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void ThrowForUnsupportedVectorBaseType<T>() where T : struct
+        {
+            if (typeof(T) != typeof(byte) && typeof(T) != typeof(sbyte) &&
+                typeof(T) != typeof(short) && typeof(T) != typeof(ushort) &&
+                typeof(T) != typeof(int) && typeof(T) != typeof(uint) &&
+                typeof(T) != typeof(long) && typeof(T) != typeof(ulong) &&
+                typeof(T) != typeof(float) && typeof(T) != typeof(double))
+            {
+                throw new Exception();
+            }
+        }
+
+        internal static void ThrowSomething(int i)
+        {
+            throw new Exception();
+        }
+    }
+}
